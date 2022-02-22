a8108ad465a99ca320800eebb4163273ee9f1e18
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2022-02-21 22:15:31 -0600

    First commit! Saludos al Ivan Frescas

---
+++ World Cup Collectibles/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  World Cup Collectibles
+//
+//  Created by José Francisco Castillo Rodríguez on 21/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ World Cup Collectibles/World_Cup_CollectiblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_Cup_CollectiblesApp.swift
+//  World Cup Collectibles
+//
+//  Created by José Francisco Castillo Rodríguez on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct World_Cup_CollectiblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
