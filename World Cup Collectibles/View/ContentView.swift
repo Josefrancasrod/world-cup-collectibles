b44d50bd97dc46e0c4cc8e120487a26631bd4fb4
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2022-02-21 22:26:25 -0600

    Solve merge problems

--- World Cup Collectibles/View/ContentView.swift
+++ World Cup Collectibles/View/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, world!")
+                .padding()
+        }
+        
     }
 }
 
