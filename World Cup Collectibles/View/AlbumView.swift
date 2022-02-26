cf6d2608715e53bebb29e2541c6f6bef0223ba12
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2022-02-25 23:55:31 -0600

    Add AlbumView and start with teams buttons

---
+++ World Cup Collectibles/View/AlbumView.swift
@@ -0,0 +1,53 @@
+//
+//  AlbumView.swift
+//  World Cup Collectibles
+//
+//  Created by José Francisco Castillo Rodríguez on 25/02/22.
+//
+
+import SwiftUI
+
+struct AlbumView: View {
+    var body: some View {
+        
+        VStack(alignment: .leading){
+            Text("World Cup\nCollectibles").font(.largeTitle).fontWeight(.bold).foregroundColor(.black).padding(.horizontal, 25)
+            ScrollView{
+                Card()
+                Card()
+                Card()
+                Card()
+                Card()
+                
+            }
+        }
+    }
+}
+
+struct AlbumView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumView()
+    }
+}
+
+struct Card:View{
+    var body: some View{
+        
+        Button(action: {
+            print("hello")
+        }, label: {
+            HStack{
+                Text("+")
+                Divider()
+                
+                VStack{
+                    Text("Team")
+                    Text("4/12")
+                }
+                Spacer()
+                Text("See more").frame(alignment: .bottomTrailing)
+            }.frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal, 50)
+        })
+        
+    }
+}
---
+++ World Cup Collectibles/View/InterchangeView.swift
@@ -0,0 +1,20 @@
+//
+//  InterchangeView.swift
+//  World Cup Collectibles
+//
+//  Created by José Francisco Castillo Rodríguez on 25/02/22.
+//
+
+import SwiftUI
+
+struct InterchangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InterchangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        InterchangeView()
+    }
+}
